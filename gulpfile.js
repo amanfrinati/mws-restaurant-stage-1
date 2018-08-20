2d5a8714af4f39f5ffedd168dd41d2d2609f071f
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2018-08-20 08:39:47 +0200

    refactor: Add package to gulp file to improve browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp');
-// var del = require('del');
+var del = require('del');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-// var eslint = require('gulp-eslint');
+var eslint = require('gulp-eslint');
 // var jasmine = require('gulp-jasmine-phantom');
 // var concat = require('gulp-concat');
 // var uglify = require('gulp-uglify');
 // var gm = require('gulp-gm');
-var browserify = require('gulp-browserify');
+// var browserify = require('gulp-browserify');
+
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var util = require('gulp-util');
 
 gulp.task('default', ['copy-html', 'copy-fonts', 'copy-images', 'styles', 'script-main', 'script-restaurant'], function() {
   gulp.watch('sass/**/*.scss', ['styles']);
@@ -16,13 +23,14 @@ gulp.task('default', ['copy-html', 'copy-fonts', 'copy-images', 'styles', 'scrip
   gulp.watch('js/*.js', ['js-watch']);
 
   browserSync.init({
-    server: './dist'
+    server: './dist',
+    notify: false
   });
 });
 
-// gulp.task('clean', function (done) {
-//   del(['dist'], done);
-// });
+gulp.task('clean', function (done) {
+  del(['dist'], done);
+});
 
 gulp.task('copy-html', () => {
   gulp.src([
@@ -35,16 +43,14 @@ gulp.task('copy-html', () => {
 
 gulp.task('copy-images', () => {
   gulp.src([
-    'images/*',
-
+    'images/*'
   ])
     .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('copy-fonts', () => {
   gulp.src([
-    'fonts/*',
-
+    'fonts/*'
   ])
     .pipe(gulp.dest('dist/fonts'));
 });
@@ -67,35 +73,34 @@ gulp.task('styles', function () {
     .pipe(browserSync.stream());
 });
 
-// gulp.task('lint', function () {
-//   return gulp.src(['js/**/*.js'])
-//     // eslint() attaches the lint output to the eslint property
-//     // of the file object so it can be used by other modules.
-//     .pipe(eslint())
-//     // eslint.format() outputs the lint results to the console.
-//     // Alternatively use eslint.formatEach() (see Docs).
-//     .pipe(eslint.format())
-//     // To have the process exit with an error code (1) on
-//     // lint error, return the stream and pipe to failOnError last.
-//     .pipe(eslint.failOnError());
-// });
+gulp.task('lint', function () {
+  return gulp.src(['js/*.js'])
+    // eslint() attaches the lint output to the eslint property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failOnError last.
+    .pipe(eslint.failOnError());
+});
 
 // Basic usage
-gulp.task('script-main', function () {
-  // Single entry point to browserify
-  gulp.src('js/main.js')
-    .pipe(browserify({
-      insertGlobals: true
-    }))
-    .pipe(gulp.dest('./dist/js'));
+gulp.task('script-main', function() {
+  return browserify({ entries: './js/main.js', debug: true })
+    // .transform(babelify)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('script-restaurant', function() {
-  gulp.src('js/restaurant_info.js')
-    .pipe(browserify({
-      insertGlobals: true
-    }))
-    .pipe(gulp.dest('./dist/js'));
+  return browserify({ entries: './js/restaurant_info.js', debug: true })
+    // .transform(babelify)
+    .bundle()
+    .pipe(source('restaurant_info.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 // create a task that ensures the `js` task is complete before
